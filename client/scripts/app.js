08f6a67f1ffa50e74ba42f96a92a5ab11d8f640d
cassac <hicassac@gmail.com>
2016-08-09 11:09:27 -0700

    adds chat room break out functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,9 @@
-var app = {};
-app.newestMessageDate = null;
-app.url = 'https://api.parse.com/1/classes/messages';
+var app = {
+  roomname: null,
+  newestMessageDate: null,
+  url: 'https://api.parse.com/1/classes/messages'
+};
+
 app.send = function(message, url) {
   var url = url || app.url;
   $.ajax({
@@ -37,13 +40,16 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message, addMethod) {
-  var $message = $('<div class="col-sm-6 col-sm-offset-4" data-roomname="' + message.roomname + '" data-objectId="' + 
+  var roomname = message.roomname || 'general';
+  var $message = $('<div data-roomname="' + roomname + '" data-objectId="' + 
     message.objectId + '"></div>');
-  $message.addClass('message');
-  var $user = $('<span>').addClass('username').text(message.username);
+  $message.addClass('chat');
+  var $user = $('<span>').addClass('username').text('@' + message.username);
   var $text = $('<span>').addClass('text').text(message.text);
   var $date = $('<span>').addClass('time').text(message.createdAt);
-  $('#chats')[addMethod]($message.append($user, $text, $date));
+  
+  var $roomname = $('<span>').addClass('roomname').text('#' + roomname);
+  $('#chats')[addMethod]($message.append($text, $user, $date, $roomname));
 };
 
 app.addRoom = function(room) {
@@ -55,27 +61,24 @@ app.addFriend = function() {
 };
 
 app.init = function() {
+  if (app.roomname) {
+    var query = 'where={"roomname":"' + app.roomname + '"}';
+  }
   app.fetch(app.url, function(data) {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < data.results.length; i++) {
       app.addMessage(data.results[i], 'append');
     }
     console.log(data);
     app.newestMessageDate = data.results[0].createdAt;
-  });
-};
-
-app.queryNewMessages = function() {
-  var date = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + app.newestMessageDate + '"}}}';
-  app.fetch(app.url, function(data) {
-    if (data.results.length > 0) {
-      $('#newMessageCountBtn').attr('disabled', false);
-      $('#newMessageCount').text(data.results.length);
-    }
-  }, date);
+  }, query);
 };
 
 app.update = function() {
-  var date = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + app.newestMessageDate + '"}}}';
+  if (app.roomname) {
+    var query = 'where={"roomname":"' + app.roomname + '","createdAt":{"$gt":{"__type":"Date","iso":"' + app.newestMessageDate + '"}}}';
+  } else {
+    var query = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + app.newestMessageDate + '"}}}';
+  }
   app.fetch(app.url, function(data) {
     for (var i = 0; i < data.results.length; i++) {
       app.addMessage(data.results[i], 'prepend');
@@ -83,24 +86,25 @@ app.update = function() {
     if (data.results.length > 0) {
       app.newestMessageDate = data.results[0].createdAt;
     }
-  }, date);
+  }, query);
 
 };
 
 app.init();
-setInterval(app.queryNewMessages, 500);
+setInterval(app.update, 500);
 
-$(document).on('click', '#newMessageCountBtn', function() {
-  app.update();
-  $('#newMessageCountBtn').attr('disabled', true);
-  $('#newMessageCount').text('');
-
+$(document).on('click', '.roomname', function(event) {
+  app.roomname = $(this).text().slice(1);
+  app.clearMessages();
+  app.init();
 });
 
 $(document).on('submit', 'form', function(event) {
   event.preventDefault();
+  var username = window.location.search.split('=');
+  username = username[username.length - 1];
   var message = {
-    username: $('input[name="username"').val(),
+    username: username,
     text: $('input[name="message"').val(),
     roomname: $('input[name="roomname"').val(),
   };
