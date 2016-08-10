ad8696657cd0cc243417507ac97e783c5f1a4552
cassac <hicassac@gmail.com>
2016-08-09 20:13:43 -0700

    adds variables for ajax headers for different requests

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -8,7 +8,12 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 }
 
 // Put your parse application keys here!
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
-});
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
+//   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+// });
+
+var parseHeaders = {'X-Parse-Application-Id': 'PARSE_APP_ID',
+                  'X-Parse-REST-API-Key': 'PARSE_API_KEY'};
+
+var botHeaders = { 'X-Mashape-Key': 'Mashape-Key' };
